07dc6b412ac80a21a1b2fdf176c832587c50d4b3
sompal ahirwar <110196012+Somahirwar18@users.noreply.github.com>
2024-08-12 21:26:21 +0530

    chatbot

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from chatbot import chat
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def echo():
+    data = request.get_json()
+    message = data['message']
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    return jsonify({"received_data": chat(message)}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
